fa6dd3327aeb9eed00bc28bc680e8e3c1ea68064
hvsio <h4sio@o2.pl>
2020-10-07 10:13:02 +0200

    Book model, resolver improvement

--- src/books/books.model.ts
+++ src/books/books.model.ts
@@ -1,4 +1,5 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { Author } from 'src/authors/author.model';
 
 @ObjectType()
 export class Book {
@@ -7,4 +8,16 @@ export class Book {
 
   @Field({ nullable: false, description: 'Book title' })
   title?: string;
+
+  @Field({ nullable: false, description: 'Book title' })
+  pageNumber?: number;
+
+  @Field({ nullable: false, description: 'Book title' })
+  published?: boolean;
+
+  @Field({ nullable: false, description: 'Book title' })
+  author?: Author;
+
+  @Field({ nullable: false, description: 'Book title' })
+  copies: number;
 }
--- src/books/books.resolver.ts
+++ src/books/books.resolver.ts
@@ -7,20 +7,65 @@ export class BooksResolver {
     {
       id: 1,
       title: 'Memórias póstumas de Brás Cubas',
+      pageNumber: 238,
+      published: true,
+      author: {
+        id: 10,
+        firstName: 'Amelia',
+        lastName: 'Kulio',
+      },
+      copies: 5789744
     },
     {
       id: 2,
       title: 'Memórias de um Sargento de Milícias',
+      pageNumber: 164,
+      published: false,
+      author: {
+        id: 11,
+        firstName: 'Jacob',
+        lastName: 'Smith',
+      },
+      copies: 28
     },
     {
       id: 3,
       title: 'O Ateneu',
+      pageNumber: 675,
+      published: true,
+      author: {
+        id: 12,
+        firstName: 'Tamara',
+        lastName: 'Einhart',
+      },
+      copies: 3793688
+    },
+    {
+      id: 4,
+      published: false,
+      author: {
+        id: 13,
+        lastName: 'Lucia',
+      },
+      copies: 0
     },
   ];
 
   @Query(returns => [Book])
   findAllBooks(): Book[] {
     return this.books;
+  }
+
+  @Query(returns => [Book])
+  findAllPublishedBooks(): Book[] {
+    const tempBooks = this.books.filter(book => book.published === true);
+    return tempBooks;
+  }
+
+  @Query(returns => [Book])
+  findLongBooks(): Book[] {
+    const tempBooks = this.books.filter(book => book.pageNumber > 400);
+    return tempBooks;
   }
 
   @Query(returns => Book)
@@ -30,9 +75,19 @@ export class BooksResolver {
 
   @Mutation(returns => Book)
   createBook(@Args('title') title: string): Book {
-    const newBook = { id: this.books.length, title };
-    [...this.books, newBook];
+    const newBook: Book = {
+      id: this.books.length,
+      title: title,
+      copies: 0
+    };
+    this.books.push(newBook);
     return newBook;
+  }
+
+  @Mutation(returns => Book)
+  updateBook(@Args('book') book: Book): Book {
+    this.books[book.id] = book;
+    return this.books[book.id];
   }
 
   @Mutation(returns => Book)
@@ -40,5 +95,10 @@ export class BooksResolver {
     const deletedBook = this.books.find(book => book.id === id);
     this.books = this.books.filter(book => book.id !== id);
     return deletedBook;
+  }
+
+  @Mutation(returns => [Book])
+  deleteAuthorsBooks(@Args('authorId') id: number): Book[] {
+    return this.books.filter(book => book.author.id === id);
   }
 }
